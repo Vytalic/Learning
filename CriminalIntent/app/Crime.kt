d8766c8666ea08a2167d5c36ea3b8175ccf2aa38
Vytalic <Vytalic>
2024-12-21 18:29:40 -0600

    Add CriminalIntent project

---
+++ CriminalIntent/app/Crime.kt
@@ -0,0 +1,6 @@
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bignerdranch.android.criminalintent.ui.theme.CriminalIntentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
